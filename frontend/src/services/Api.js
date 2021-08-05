66bae203cfa1179ce4319660a02ea43dc4989d4a
CurtiePi <curtisbklyn@gmail.com>
2021-08-05 16:56:27 -0400

    Use store to load request header with Authorization value

--- frontend/src/services/Api.js
+++ frontend/src/services/Api.js
@@ -1,7 +1,18 @@
 import axios from 'axios'
+import store from '../store'
 
-export default  () => {
-  return axios.create({
-      baseURL: 'http://192.168.1.3:3020'
-  })
-}
+const baseURL = 'http://192.168.1.3:3020'
+
+
+let axiosConnection = axios.create({
+  baseURL,
+})
+
+axiosConnection.interceptors.request.use((request) => {
+    if (store.getters.isAuth) {
+      request.headers['Authorization'] = store.getters.getToken
+    }
+    return request
+})
+
+export default axiosConnection
--- frontend/src/services/InformationService.js
+++ frontend/src/services/InformationService.js
@@ -2,9 +2,9 @@ import Api from '@/services/Api'
 
 export default {
   queryInfo (payload) {
-    return Api().post('/graphql', payload)
+    return Api.post('/graphql', payload)
   },
   mutateInfo (payload) {
-    return Api().post('/graphql', payload)
+    return Api.post('/graphql', payload)
   }
 }
