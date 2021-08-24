30b6a61ae04eab8ed45735f08143acbfbbfa9af7
CurtiePi <curtisbklyn@gmail.com>
2021-08-24 15:12:07 -0400

    Added a filter for the contact list

--- frontend/src/components/ContactList.vue
+++ frontend/src/components/ContactList.vue
@@ -1,13 +1,36 @@
 <template>
   <div class="container">
     <h2>Contacts</h2>
+    <div class="row filter-div">
+      Contacts Filter
+      <span class="status">
+        <label>
+          <input type="radio" name="contact_filter" value="all" 
+            @change="filterContacts()"
+            v-model="contacts_view" />
+            All ({{ contactList.length }})
+        </label>
+        <label>
+          <input type="radio" name="contact_filter" value="messaged"
+            @change="filterContacts()"
+            v-model="contacts_view" />
+            Messaged ({{ messagedCount }})
+        </label>
+        <label>
+          <input type="radio" name="contact_filter" value="unmessaged" 
+            @change="filterContacts()"
+            v-model="contacts_view" />
+            Unmessaged ({{ unmessagedCount }})
+        </label>
+      </span>
+    </div>
     <div>
       <tr>
         <th>Name</th>
         <th>Email</th>
         <th>About Me</th>
       </tr>
-      <tr v-for="contact in contactList"
+      <tr v-for="contact in contacts_display"
           :key="contact.id">
         <td width="10%">{{ contact.name }}</td>
         <td width="20%">{{ contact.email }}</td>
@@ -23,7 +46,17 @@ export default {
   name: 'ContactList',
   data () {
     return {
-      contactList: []
+      contactList: [],
+      contacts_display: [],
+      contacts_view: 'unmessaged',
+    }
+  },
+  computed: {
+    messagedCount () {
+      return this.contactList.filter((contact) => { return contact.hasMessaged }).length
+    },
+    unmessagedCount () {
+      return this.contactList.filter((contact) => { return !contact.hasMessaged }).length
     }
   },
   methods: {
@@ -35,6 +68,7 @@ export default {
             name
             email
             cover
+            hasMessaged
           }
         }`
       }
@@ -42,10 +76,24 @@ export default {
       try{
         let result = await InformationService.queryInfo(payload)
         this.contactList = result.data.data.contacts
+        this.filterContacts()
       } catch (err) {
         console.log(err)
       }
-    }
+    },
+    filterContacts () {
+      switch (this.contacts_view) {
+        case 'all': 
+          this.contacts_display = this.contactList
+          break
+        case 'messaged': 
+          this.contacts_display = this.contactList.filter((contact) => { return contact.hasMessaged })
+          break
+        case 'unmessaged':
+          this.contacts_display = this.contactList.filter((contact) => { return !contact.hasMessaged })
+          break
+      }
+    },
   },
   mounted () {
     this.getContacts()
