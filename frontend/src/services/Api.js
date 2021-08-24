07e113da2759a388b746ab84f277247f3d56aa48
CurtiePi <curtisbklyn@gmail.com>
2021-08-24 11:38:41 -0400

    Updated server url

--- frontend/src/services/Api.js
+++ frontend/src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import store from '../store'
 
-const baseURL = 'http://192.168.1.3:3020'
+const baseURL = 'http://192.168.1.5:3020'
 
 
 let axiosConnection = axios.create({
