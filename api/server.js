a1343e2f36da453f1c204dd8c71f567f84ff64bc
CurtiePi <curtisbklyn@gmail.com>
2021-08-25 18:34:55 -0400

    Application uses the email functionality

--- api/server.js
+++ api/server.js
@@ -8,14 +8,12 @@ const models            = require('../middleware/inject_models');
 
 app.use(cors({credentials: true, origin: 'http://192.168.1.5:8080'}));
 
-// simple route
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to the party Hans."});
-});
 
+emailRouter = require('../routes/mail_route');
 app.use(isAuth);
 app.use(models);
 
+app.use('/email', emailRouter);
 app.use('/graphql', graphqlHTTP({
     schema: schema,
     pretty: true
