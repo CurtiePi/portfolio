d58b3972a336144f4d36f54a503d884b9fb0d29f
CurtiePi <curtisbklyn@gmail.com>
2021-08-25 18:33:55 -0400

    Email Signature information

---
+++ signature.js
@@ -0,0 +1,9 @@
+
+const name = '<br/><span style="font-family: Arial; color: #436663; font-size: 1.75em">Kecia Hayes</span>';
+const email = '<a href="mailto:lenoxquiche@gmail.com">lenoxquiche@gmail.com</a>';
+const phone = '646-283-1120';
+const site = '<a href="www.google.com">www.google.com</a>';
+
+signature = `${name}<br/>${email}<br/>${phone}<br/>${site}`;
+
+module.exports = signature;
