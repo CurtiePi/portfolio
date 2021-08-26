7f49b369ea8b6c226243e528950edca2fe152c14
CurtiePi <curtisbklyn@gmail.com>
2021-08-26 16:10:01 -0400

    Application to use routes before using models

--- api/server.js
+++ api/server.js
@@ -1,19 +1,21 @@
 const express           = require("express");
 const { graphqlHTTP }   = require('express-graphql');
+const bodyParser        = require('body-parser')
 const schema            = require('../graphql/schema');
 const cors              = require("cors");
 const app               = express();
 const isAuth            = require('../middleware/auth_check');
 const models            = require('../middleware/inject_models');
 
+var jsonParser = bodyParser.json();
+
 app.use(cors({credentials: true, origin: 'http://192.168.1.5:8080'}));
 
 
-emailRouter = require('../routes/mail_route');
+const emailRouter = require('../routes/mail_route');
 app.use(isAuth);
+app.use('/email', jsonParser, emailRouter);
 app.use(models);
-
-app.use('/email', emailRouter);
 app.use('/graphql', graphqlHTTP({
     schema: schema,
     pretty: true
